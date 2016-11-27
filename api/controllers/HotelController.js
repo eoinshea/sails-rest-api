2c79cb734633fbe3c01dd1aded397c302771bba7
eoin <Eoin O>
2016-11-27 10:36:34 +0000

    change stuff

---
+++ api/controllers/HotelController.js
@@ -0,0 +1,11 @@
+/**
+ * HotelController
+ *
+ * @description :: Server-side logic for managing hotels
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
